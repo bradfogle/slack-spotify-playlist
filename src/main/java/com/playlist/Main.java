6f96d788894537e31afdf3107dfe10bb0883b744
Brad Fogle <bradley.fogle@gmail.com>
2015-07-23 10:22:42 -0700

    Changed mechanism for finding new songs. Now comparing list from Spotify with last persisted list in Redis. Any diff is considered a new song and added to Slack.

--- src/main/java/com/playlist/Main.java
+++ src/main/java/com/playlist/Main.java
@@ -7,11 +7,6 @@ import com.wrapper.spotify.models.Playlist;
 import com.wrapper.spotify.models.PlaylistTrack;
 import com.wrapper.spotify.models.User;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -26,49 +21,21 @@ public class Main {
     final static String userId = System.getenv("SPOTIFY_USER_ID");
     final static String playlistId = System.getenv("SPOTIFY_PLAYLIST_ID");
 
-    final static DateFormat format = new SimpleDateFormat("MM/dd/yyyy");
-    private static Date startDate = new Date();
-
     public static void main(String[] args) {
         try {
-            startDate = format.parse(System.getenv("START_DATE"));
-
             while(true) {
                 Api spotifyApi = spotifyClient.getSpotifyApi();
                 spotifyApi.getPlaylistTracks(userId, playlistId);
 
                 PlaylistRequest request = spotifyApi.getPlaylist(userId, playlistId).build();
 
-                //Check if we've stored a newer updated time (from a playlist update) otherwise use default
-                Date lastUpdatedTime = redisClient.getLastUpdatedTime() != null ? redisClient.getLastUpdatedTime() : startDate;
-
                 try {
                     Playlist playlist = request.get();
-                    List<PlaylistTrack> playlistTracks = playlist.getTracks().getItems();
-
-                    //Reverse order of items to speed up new track lookup
-                    Collections.reverse(playlistTracks);
-
-                    Date newLastUpdatedTime = lastUpdatedTime;
-
-                    //Loop through playlist tracks, starting with most recently added
-                    for(PlaylistTrack t : playlistTracks) {
-                        //If current track was added after last execution
-                        if(lastUpdatedTime.before(t.getAddedAt())) {
-                            postToSlack(spotifyApi, playlist, t);
-
-                            //If processing multiple tracks, calculate newLastUpdatedTime based on latest addition
-                            if(newLastUpdatedTime.before(t.getAddedAt())) {
-                                newLastUpdatedTime = t.getAddedAt();
-                            }
-                        }
+                    List<PlaylistTrack> newTracks = redisClient.getNewPlaylistTracks(playlist);
+                    for(PlaylistTrack t : newTracks) {
+                        postToSlack(spotifyApi, playlist, t);
+                        redisClient.addNewTrack(t);
                     }
-
-                    //Update last updated time with latest playlist track addition time
-                    if(lastUpdatedTime.before(newLastUpdatedTime)) {
-                        redisClient.saveLastUpdatedTime(newLastUpdatedTime);
-                    }
-
                 } catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
@@ -78,8 +45,6 @@ public class Main {
             }
         } catch (InterruptedException ie) {
             System.out.println("Thread interrupted!!");
-        } catch (ParseException pe) {
-            System.out.println("Unable to parse date: " + System.getenv("START_DATE"));
         }
     }
 
@@ -98,8 +63,8 @@ public class Main {
         String trackUrl = track.getTrack().getExternalUrls().get("spotify");
 
         String message = String.format("New track added to <%s|%s> by <%s|%s>: <%s|(link)>", playlist.getUri(), playlist.getName(), user.getUri(), addedBy, trackUrl);
-
         System.out.println(message);
+
         SlackClient slackClient = new SlackClient();
         try {
             slackClient.postMessageToSlack(message);
--- src/main/java/com/playlist/RedisClient.java
+++ src/main/java/com/playlist/RedisClient.java
@@ -1,9 +1,12 @@
 package com.playlist;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wrapper.spotify.models.Playlist;
 import com.wrapper.spotify.models.PlaylistTrack;
 import redis.clients.jedis.Jedis;
 
-import java.util.Date;
+import java.io.StringWriter;
+import java.util.*;
 
 /**
  * User: bradfogle
@@ -12,22 +15,55 @@ import java.util.Date;
  */
 public class RedisClient {
 
-    private final String redisHost = "";
+    private final String redisHost = System.getenv("REDIS_HOST");
     private Jedis client;
-
-    private Date lastUpdatedTime = new Date(1432240043L);
 
     public RedisClient() {
         this.client = new Jedis(redisHost);
     }
 
-    public void saveLastUpdatedTime(Date lastUpdatedTime) {
-//        client.set("last_updated", lastUpdatedTime.toString());
-        this.lastUpdatedTime = lastUpdatedTime;
+    public List<String> getRedisPlaylist() {
+        Set<String> playlistSet = client.smembers("playlist");
+        List<String> playlistTrackList = new ArrayList<String>(playlistSet);
+        return playlistTrackList;
     }
 
-    public Date getLastUpdatedTime() {
-//        return new Date(new Long(client.get("last_updated")));
-        return lastUpdatedTime;
+    public List<PlaylistTrack> getNewPlaylistTracks(Playlist playlist) {
+        List<String> redisPlaylistList = getRedisPlaylist();
+        List<PlaylistTrack> spotifyPlaylistTracks = playlist.getTracks().getItems();
+
+        ObjectMapper mapper = new ObjectMapper();
+        List<String> newAdditionsList = new ArrayList<String>();
+
+        try {
+            for(PlaylistTrack t : spotifyPlaylistTracks) {
+                StringWriter sw = new StringWriter();
+                mapper.writeValue(sw, t);
+                newAdditionsList.add(sw.toString());
+            }
+        } catch (Exception e) {
+            System.out.println("Mapping exception: " + e.getMessage());
+        }
+
+        newAdditionsList.removeAll(redisPlaylistList);
+
+        //Attempt to rebuild playlist track list with new songs
+        try {
+            spotifyPlaylistTracks.clear();
+            for(String s : newAdditionsList) {
+                spotifyPlaylistTracks.add(mapper.readValue(s, PlaylistTrack.class));
+            }
+        } catch (Exception e) {
+            System.out.println("Mapping exception: " + e.getMessage());
+        }
+
+        return spotifyPlaylistTracks;
+    }
+
+    public void addNewTrack(PlaylistTrack track) throws Exception {
+        StringWriter playlistJson = new StringWriter();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(playlistJson, track);
+        client.sadd("playlist", playlistJson.toString());
     }
 }
