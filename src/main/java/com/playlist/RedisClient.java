579780225827498925392c6ff4fbb58803b4fa4e
Brad Fogle <bradley.fogle@gmail.com>
2015-07-23 22:46:20 -0700

    Pulled getNewPlaylistTracks method out and moved to Main.

--- src/main/java/com/playlist/RedisClient.java
+++ src/main/java/com/playlist/RedisClient.java
@@ -1,17 +1,16 @@
 package com.playlist;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.wrapper.spotify.models.Playlist;
 import com.wrapper.spotify.models.PlaylistTrack;
 import redis.clients.jedis.Jedis;
 
 import java.io.StringWriter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 /**
- * User: bradfogle
- * Date: 7/21/15
- * Time: 1:12 PM
+ * Helper class built on the Jedis project for persisting and retrieving the playlist as a set.
  */
 public class RedisClient {
 
@@ -26,38 +25,6 @@ public class RedisClient {
         Set<String> playlistSet = client.smembers("playlist");
         List<String> playlistTrackList = new ArrayList<String>(playlistSet);
         return playlistTrackList;
-    }
-
-    public List<PlaylistTrack> getNewPlaylistTracks(Playlist playlist) {
-        List<String> redisPlaylistList = getRedisPlaylist();
-        List<PlaylistTrack> spotifyPlaylistTracks = playlist.getTracks().getItems();
-
-        ObjectMapper mapper = new ObjectMapper();
-        List<String> newAdditionsList = new ArrayList<String>();
-
-        try {
-            for(PlaylistTrack t : spotifyPlaylistTracks) {
-                StringWriter sw = new StringWriter();
-                mapper.writeValue(sw, t);
-                newAdditionsList.add(sw.toString());
-            }
-        } catch (Exception e) {
-            System.out.println("Mapping exception: " + e.getMessage());
-        }
-
-        newAdditionsList.removeAll(redisPlaylistList);
-
-        //Attempt to rebuild playlist track list with new songs
-        try {
-            spotifyPlaylistTracks.clear();
-            for(String s : newAdditionsList) {
-                spotifyPlaylistTracks.add(mapper.readValue(s, PlaylistTrack.class));
-            }
-        } catch (Exception e) {
-            System.out.println("Mapping exception: " + e.getMessage());
-        }
-
-        return spotifyPlaylistTracks;
     }
 
     public void addNewTrack(PlaylistTrack track) throws Exception {
