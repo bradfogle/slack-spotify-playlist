7d082c30cb420615719b96de2cf8d3af60af232f
Brad Fogle <bradley.fogle@gmail.com>
2015-07-29 10:50:11 -0700

    Adding connection pooling.
    Implementing a JedisPool for managing connections and adding connection closing after each call to Redis.

--- src/main/java/com/playlist/RedisClient.java
+++ src/main/java/com/playlist/RedisClient.java
@@ -3,6 +3,8 @@ package com.playlist;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wrapper.spotify.models.PlaylistTrack;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 import java.io.StringWriter;
 import java.net.URI;
@@ -17,26 +19,45 @@ import java.util.Set;
 public class RedisClient {
 
     private Jedis client;
+    private JedisPool pool;
 
     public RedisClient() {
-
         try {
-            this.client = new Jedis(new URI(System.getenv("REDISTOGO_URL")));
+            JedisPoolConfig config = new JedisPoolConfig();
+            config.setMaxTotal(1);
+            this.pool = new JedisPool(config, new URI(System.getenv("REDISTOGO_URL")));
         } catch (URISyntaxException e) {
             System.out.println("Error parsing Redis URI: " + e.getMessage());
         }
     }
 
     public List<String> getRedisPlaylist() {
-        Set<String> playlistSet = client.smembers("playlist");
-        List<String> playlistTrackList = new ArrayList<String>(playlistSet);
+        List<String> playlistTrackList;
+
+        try {
+            client = pool.getResource();
+            Set<String> playlistSet = client.smembers("playlist");
+            playlistTrackList = new ArrayList<String>(playlistSet);
+        } finally {
+            if(client != null) {
+                client.close();
+            }
+        }
+
         return playlistTrackList;
     }
 
     public void addNewTrack(PlaylistTrack track) throws Exception {
-        StringWriter playlistJson = new StringWriter();
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.writeValue(playlistJson, track);
-        client.sadd("playlist", playlistJson.toString());
+        try {
+            client = pool.getResource();
+            StringWriter playlistJson = new StringWriter();
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(playlistJson, track);
+            client.sadd("playlist", playlistJson.toString());
+        } finally {
+            if(client != null) {
+                client.close();
+            }
+        }
     }
 }
